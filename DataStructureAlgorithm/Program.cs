b8b98569a2f01e898141c051aad3b02a6a1ffc95
shwethab8888 <shwethab8888@gmail.com>
2023-03-23 23:23:25 +0530

    Binary Search the Word from Word List

---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DataStructureAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Data structure algorithms");
+            BinarySearch.Binary_Search();
+        
+        }
+    }
+}
