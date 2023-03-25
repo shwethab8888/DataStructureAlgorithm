845ff7be7fe4d8c758751b5350bf964a5d5abd11
shwethab8888 <122897911+shwethab8888@users.noreply.github.com>
2023-03-25 08:10:01 +0530

    Update BinarySearch.cs

--- DataStructureAlgorithm/BinarySearch.cs
+++ DataStructureAlgorithm/BinarySearch.cs
@@ -17,40 +17,30 @@ namespace DataStructureAlgorithm
                 int result = x.CompareTo(arr[mid]);
                 if (result == 0)
                 {
-                    // If x is found at mid, return mid
                     return mid;
                 }
                 else if (result < 0)
                 {
-                    // If x is smaller than arr[mid], ignore right half
                     right = mid - 1;
                 }
                 else
                 {
-                    // If x is greater than arr[mid], ignore left half
                     left = mid + 1;
                 }
             }
-            // If we reach here, then the element was not present in the array
             return -1;
         }
 
         public static void Binary_Search()
         {
-            // Read in the list of words from file
             string[] words = File.ReadAllText("words.txt").Split(',');
 
-            // Sort the words using Array.Sort() method
             Array.Sort(words);
 
-            // Prompt the user to enter a word to search for
             Console.Write("Enter a word to search for: ");
             string searchWord = Console.ReadLine();
-
-            // Perform Binary Search using the BinarySearch.Search() method
             int index = BinarySearch.Search(words, searchWord);
 
-            // Check if the word was found
             if (index >= 0)
             {
                 Console.WriteLine("The word {0} was found at index {1}.", searchWord, index);
